6ccece59736263f2e88f1fa138a73d29589d72e8
Eduardo <eduardobarbaro.ifc@gmail.com>
2022-05-06 21:47:25 -0300

    master

--- src/components/BlocoCabecalho.vue
+++ src/components/BlocoCabecalho.vue
@@ -4,12 +4,12 @@ export default {};
 
 <template>
     <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand">Só Cifras</a>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="O que você quer tocar hoje?" aria-label="Pesquisar">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-  </form>
-</nav>
+        <a class="navbar-brand">Só Cifras</a>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="O que você quer tocar hoje?" aria-label="Pesquisar">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+        </form>
+    </nav>
 </template>
 
 <style></style>
